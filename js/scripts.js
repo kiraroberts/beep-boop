903d481cc7cce4193d30cc6f46dce1ca4bf8dbe4
Kira <rosehill1788@protonmail.com>
2019-06-14 12:16:04 -0700

    Add back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,31 @@ $(document).ready(function () {
   $('form.inputNumberForm').click(function (event) {
     event.preventDefault();
 
-    //var userInput = [];
+    var userInput = [($('input#inputNumber').val())];
+    console.log(userInput);
 
-    var userInput = ($('input#inputNumber').val());
-
-
-    for (var index = userInput; index >= 0; index -= 1) {
-      var halErrors = [1, 2, 3];
-
-       if (halErrors.includes(1)(function(index))) {
-       userInput.splice(index, 1, "beep");
-     } else if (halErrors.includes(2)(index)) {
-       userInput.splice(index, 1, "boop");
-     } else (halErrors.includes(3)(index))
-       userInput.splice(index, 1, "I'm sorry Dave. I'm afraid I can't do that.");
-    console.log(userInput[index]);
+    for (index = userInput; index >= 0; index += 1) {
+      const map1 = userInput.map(X => [userInput] - 1);
+      userInput.push(map1);
+      console.log(userInput[1]);
     };
   });
 });
+
+
+
+
+
+
+
+
+
+// for (var index = userInput; index >= 0; index -= 1) {
+  //   var halErrors = [1, 2, 3];
+  //
+  //    if (halErrors.includes(1)([index])) {
+    //    userInput.splice(index, 1, "beep");
+    //  } else if (halErrors.includes(2)(index)) {
+      //    userInput.splice(index, 1, "boop");
+      //  } else (halErrors.includes(3)(index))
+      //    userInput.splice(index, 1, "I'm sorry Dave. I'm afraid I can't do that.");
