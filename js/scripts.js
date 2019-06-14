a91a48b8f156fae2bb268dacf8363d11f2c1c24e
Kira <rosehill1788@protonmail.com>
2019-06-14 11:46:59 -0700

    Add back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 // BACK END
 
-var userInput = [];
-$('#result').empty();
-var userInput = ($('#inputNumber').val()).split('');
-console.log(userInput)
+
+// $('#result').empty();
 
 
 // FRONT END
 
 $(document).ready(function () {
-  $('form.inputNumber').submit(function (event) {
+  $('form.inputNumberForm').click(function (event) {
     event.preventDefault();
+
+    //var userInput = [];
+
+    var userInput = ($('input#inputNumber').val());
+
+
+    for (var index = userInput; index >= 0; index -= 1) {
+      var halErrors = [1, 2, 3];
+
+       if (halErrors.includes(1)(function(index))) {
+       userInput.splice(index, 1, "beep");
+     } else if (halErrors.includes(2)(index)) {
+       userInput.splice(index, 1, "boop");
+     } else (halErrors.includes(3)(index))
+       userInput.splice(index, 1, "I'm sorry Dave. I'm afraid I can't do that.");
+    console.log(userInput[index]);
+    };
   });
 });
