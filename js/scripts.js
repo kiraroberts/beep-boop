f2e183ee956cd02f82fe30e0fa6bc8483e051920
Kira <rosehill1788@protonmail.com>
2019-06-14 16:37:56 -0700

    Rework for loops, if else, and add css styling, html elements

--- js/scripts.js
+++ js/scripts.js
@@ -11,23 +11,25 @@ $(document).ready(function () {
     event.preventDefault();
 
     var userInput = ($('input#inputNumber').val());
+    var arr1 = [];
+    var arr2 = [];
 
-    for (int i = 0; i <= userInput; i++) {
-    var x = i.toString();
-      for (int y = 0; y < x.length(); y++) {
-      var z = x.charAt(y);
-        if (z = "3") {
-          return "I'm sorry Dave.";
-      } else if (z = "2") {
-          return "boop";
-      } else if (z = "1") {
-          return "beep";
+    for (var i = 0; i <= userInput; i++) {
+      var x = i.toString();
+      for (var y = 0; y < x.length; y++) {
+      var z = parseInt((i + '').charAt(0))
+        arr1.push(z)
+        if (z === "3") {
+          var output =  "I'm sorry, Dave. I'm afraid I can't do that.";
+      } else if (z === "2") {
+          var output = "boop";
+      } else if (z === "1") {
+          var output =  "beep";
       } else {
-          return i;
+          var output = i;
       }
-
-
-    console.log(userInput);
+      arr1.push(output);
+      console.log(arr1);
     };
     };
   });
