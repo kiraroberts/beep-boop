6136be84bcc5e618fd88c4939a81fda0bea7e9b1
Kira <rosehill1788@protonmail.com>
2019-06-20 20:04:11 -0700

    Revise for loops

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,29 @@
 $(document).ready(function () {
   $('form.inputNumberForm').click(function (event) {
     event.preventDefault();
-
-    var userInput = ($('input#inputNumber').val());
     var arr1 = [];
     var arr2 = [];
+    var userInput = ($('input#inputNumber').val());
+
+
 
     for (var i = 0; i <= userInput; i++) {
       var x = i.toString();
-      for (var y = 0; y < x.length; y++) {
-      var z = parseInt((i + '').charAt(0))
-        arr1.push(z)
-        if (z === "3") {
-          var output =  "I'm sorry, Dave. I'm afraid I can't do that.";
-      } else if (z === "2") {
-          var output = "boop";
-      } else if (z === "1") {
-          var output =  "beep";
+      arr1.push(x)
+    };
+
+      arr1.forEach(function(g) {
+        if ((g.includes("3"))) {
+          arr2.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if ((g.includes("2"))) {
+          arr2.push("boop");
+      } else if ((g.includes("1"))) {
+          arr2.push("beep");
       } else {
-          var output = i;
+          arr2.push(g);
       }
-      arr1.push(output);
-      console.log(arr1);
-    };
-    };
+    });
+    console.log(arr2);
   });
 });
 
