f9f5643a0e843cb056b51f926999cdae7f06ca5f
Kira <rosehill1788@protonmail.com>
2019-06-26 21:54:33 -0700

    Refactor backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,37 @@
 // BACK END
+var arr1 = [];
+var arr2 = [];
 
+function beeper(userInput) {
+  for (var i = 0; i <= userInput; i++) {
+    var x = i.toString();
+    arr1.push(x)
+  };
 
+  arr1.forEach(function(number) {
+    if ((number.includes("3"))) {
+      arr2.push(" I'm sorry, Dave. I'm afraid I can't do that.")
+    } else if ((number.includes("2"))) {
+      arr2.push(" boop");
+    } else if ((number.includes("1"))) {
+      arr2.push(" beep");
+    } else {
+      arr2.push(number);
+    }
+  })
+  console.log(arr2);
+  return arr2
+};
 
 // FRONT END
 
 $(document).ready(function () {
-  $('form.inputNumberForm').click(function (event) {
+  $('form.inputNumberForm').submit(function (event) {
     event.preventDefault();
-    var arr1 = [];
-    var arr2 = [];
     var userInput = ($('input#inputNumber').val());
 
+    var output = beeper(userInput);
 
-
-    for (var i = 0; i <= userInput; i++) {
-      var x = i.toString();
-      arr1.push(x)
-    };
-
-      arr1.forEach(function(g) {
-        if ((g.includes("3"))) {
-          arr2.push(" I'm sorry, Dave. I'm afraid I can't do that.")
-      } else if ((g.includes("2"))) {
-          arr2.push(" boop");
-      } else if ((g.includes("1"))) {
-          arr2.push(" beep");
-      } else {
-          arr2.push(g);
-      }
-    });
-    $("h1#result").text(arr2);
+    $("h1#result").text(output);
   });
 });
